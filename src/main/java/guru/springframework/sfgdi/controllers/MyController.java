93946e1cc424687c5b5aa222bb92e436107d3838
anand <ananda11k@gmail.com>
2020-10-15 14:40:16 +0530

    Add a simple controller & demostrate the use of app context to retrieve controller

--- src/main/java/guru/springframework/sfgdi/controllers/MyController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/MyController.java
@@ -1,2 +1,12 @@
-package guru.springframework.sfgdi.controllers;public class MyController {
+package guru.springframework.sfgdi.controllers;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyController {
+
+    public String sayHello() {
+        System.out.println("Hello World!");
+        return "Hi Folks!";
+    }
 }
